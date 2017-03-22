c86a83e7840b3c14c45dee12bc0f400a289f07ed
Roger Zhang <Roger.Zhang.Eng@gmail.com>
2017-03-22 23:45:33 +1100

    add network model code.

---
+++ Exercise1/Model/NetworkModel.swift
@@ -0,0 +1,98 @@
+//
+//  NetworkModel.swift
+//  Exercise1
+//
+//  Created by Roger Zhang on 22/3/17.
+//  Copyright © 2017 Roger.Zhang. All rights reserved.
+//
+
+import Foundation
+
+
+
+/*
+ {
+ "meta":{
+ "code":200,
+ "requestId":"58d270bdf594df18b3a3cc37"
+ },
+ "response":{
+ "venues":[
+ {
+ "id":"4b5aa658f964a5206bcf28e3",
+ "name":"Gloria Jean's Coffees",
+ "contact":{
+ "phone":"+61433899996",
+ "formattedPhone":"+61 433 899 996",
+ "twitter":"gloriajeans"
+ },
+ "location":{
+ "address":"The Galleries Victoria\rCnr Pitt & Park Streets",
+ "crossStreet":"cnr Pitt St. & Park St.",
+ "lat":-33.87294542928269,
+ "lng":151.2079882621765,
+ "labeledLatLngs":[
+ {
+ "label":"display",
+ "lat":-33.87294542928269,
+ "lng":151.2079882621765
+ }
+ ],
+ "distance":113,
+ "postalCode":"2000",
+ "cc":"AU",
+ "city":"Sydney",
+ "state":"NSW",
+ "country":"Australia",
+ "formattedAddress":[
+ "The Galleries Victoria\rCnr Pitt & Park Streets (cnr Pitt St. & Park St.)",
+ "Sydney NSW 2000",
+ "Australia"
+ ]
+ },
+ "categories":[
+ {
+ "id":"4bf58dd8d48988d1e0931735",
+ "name":"Coffee Shop",
+ "pluralName":"Coffee Shops",
+ "shortName":"Coffee Shop",
+ "icon":{
+ "prefix":"https:\/\/ss3.4sqi.net\/img\/categories_v2\/food\/coffeeshop_",
+ "suffix":".png"
+ },
+ "primary":true
+ }
+ ],
+ "verified":true,
+ "stats":{
+ "checkinsCount":8486,
+ "usersCount":3689,
+ "tipCount":90
+ },
+ "url":"http:\/\/www.gloriajeanscoffees.com",
+ "allowMenuUrlEdit":true,
+ "beenHere":{
+ "lastCheckinExpiredAt":0
+ },
+ "specials":{
+ "count":0,
+ "items":[
+ ]
+ },
+ "hereNow":{
+ "count":0,
+ "summary":"Nobody here",
+ "groups":[
+ ]
+ },
+ "referralId":"v-1490186430",
+ "venueChains":[
+ ],
+ "hasPerk":false
+ },
+ ... multiple items ... 
+ ] //end of venues
+ } //end of response
+ }
+ 
+ */
